c3c23e840e6a2da74d3272326b4eaaaa35173cce
Giles Somerville <giles.somerville@sharecare.com>
2020-10-07 14:44:31 -0400

    chore: auto format canonical.spec.ts

--- src/__tests__/canonical.spec.ts
+++ src/__tests__/canonical.spec.ts
@@ -1,20 +1,18 @@
-import { R4 } from '../index';
+import { R4 } from "../index";
 
 const T = R4.RTTI_canonical;
 const value = "http://snomed.info/sct";
-        
-describe('Scalar canonocal', () => {
-  
-    test("should decode a valid value", () => {        
-        expect(T.decode(value)._tag).toBe('Right');
-      });
-    
-      test("should return the same reference if succeeded", () => {
-        expect(T.decode(value).value).toEqual(value);
-      });
-    
-      test("Should not decode invalid value", () => {
-        expect(T.decode(2)._tag).toBe('Left');
-      });
-    
-})
+
+describe("Scalar canonocal", () => {
+  test("should decode a valid value", () => {
+    expect(T.decode(value)._tag).toBe("Right");
+  });
+
+  test("should return the same reference if succeeded", () => {
+    expect(T.decode(value).value).toEqual(value);
+  });
+
+  test("Should not decode invalid value", () => {
+    expect(T.decode(2)._tag).toBe("Left");
+  });
+});
